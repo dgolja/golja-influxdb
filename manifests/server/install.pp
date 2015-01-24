f5e36f2363847d336eeef02c708964be53ae0068
Dejan Golja <dejan@golja.org>
2015-01-24 21:18:19 +1100

    fix Exec path

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,17 +1,20 @@
 class influxdb::server::install {
   $version = $influxdb::server::version
-  
+
+  Exec {
+    path => '/usr/bin:/bin',
+  }
+
+  # Until they release a proper repository we will need to do with that
+  exec {'get_influxdb':
+    command => "curl -s ${influxdb::params::package_source}${version}${influxdb::params::package_suffix} -o /tmp/influxdb_${version}${influxdb::params::package_suffix} && touch /tmp/.get_influxdb_${version}",
+    creates => "/tmp/.get_influxdb_${version}",
+  } ->
+
   package {'influxdb':
     ensure   => $influxdb::server::package_ensure,
     provider => $influxdb::server::package_provider,
     source   => "/tmp/influxdb_${version}${influxdb::params::package_suffix}",
-  }
-
-  # hopefully soon they will have an proper repo
-  exec {'get_influxdb':
-    command => "/usr/bin/curl ${influxdb::params::package_source}${version}${influxdb::params::package_suffix} -o /tmp/influxdb_${version}${influxdb::params::package_suffix} && touch /tmp/.get_influxdb_${version}",
-    creates => "/tmp/.get_influxdb_${version}",
-    before  => Package['influxdb'],
   }
 
 }
