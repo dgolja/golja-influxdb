7f3b520b9b57bd53b3ea777802fa0c45341f3b72
Dejan Golja <dejan@golja.org>
2016-03-05 14:02:17 +1100

    Revert back to the old puppetlabs/apt syntax so even the users with the old puppetlabs/apt modules (prior to 2.X) can use this module.

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -6,16 +6,12 @@ class influxdb::repo::apt {
   $_operatingsystem = downcase($::operatingsystem)
 
   apt::source { 'repos.influxdata.com':
-    location => "https://repos.influxdata.com/${_operatingsystem}",
-    release  => $::lsbdistcodename,
-    repos    => 'stable',
-    include  => {
-      'src' => false
-    },
-    key      => {
-      'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-      'source' => 'https://repos.influxdata.com/influxdb.key'
-    },
+    location    => "https://repos.influxdata.com/${_operatingsystem}",
+    release     => $::lsbdistcodename,
+    repos       => 'stable',
+    key         => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
+    key_source  => 'https://repos.influxdata.com/influxdb.key',
+    include_src => false,
   }
 
   Apt::Source['repos.influxdata.com'] -> Package<| tag == 'influxdb' |>
