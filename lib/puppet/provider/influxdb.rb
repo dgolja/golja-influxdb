9ce0c9930140db065d9ce4828b92cd828f0591e4
Tobias Urdin <tobias.urdin@binero.se>
2019-01-20 00:11:32 +0100

    Add InfluxDB base provider
    Adds a base InfluxDB provider that can talk
    to the InfluxDB API.

---
+++ lib/puppet/provider/influxdb.rb
@@ -0,0 +1,127 @@
+# Copyright (C) 2018 Binero
+#
+# Author: Tobias Urdin <tobias.urdin@binero.se>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+require 'net/http'
+require 'json'
+
+class Puppet::Provider::InfluxDB < Puppet::Provider
+  # @abstract Subclass is expected to implement #base_url
+  # @method base_url
+  #   The base URL to InfluxDB API
+  #
+  #   Normally we don't want to do this but it's
+  #   easier to stub in testing.
+  def self.base_url
+    raise NotImplementedError
+  end
+
+  # @method parse_url
+  #   Parse the base URL and fix the path that will be used for the requests.
+  #   Protected function that can only used by this class and subclasses that
+  #   inherits this class.
+  # @param path
+  #   The path to add to the base URL, must start with a slash
+  #   or URI will throw an exception.
+  # @param query
+  #   Optional parameter if the query parameters that should be added to
+  #   the URL.
+  protected
+  def self.parse_url(path, query=nil)
+    url = URI.parse(base_url)
+    path.prepend('/') unless path[0] == '/'
+    url.path = URI.escape(path)
+    url.query = URI.escape(query) if query
+    url
+  end
+
+  # @method query
+  #   Run a query against the InfluxDB API
+  # @param query
+  #   The query to run
+  def self.query(query)
+    q = "q=#{query}"
+    post('query', q)
+  end
+
+  # @method get
+  #   Perform a GET request
+  # @param path
+  #   The path to perform a GET request against
+  # @param query
+  #   The query to add to the URL
+  # @param data
+  #   The data to send with the request
+  # @param headers
+  #   The headers to send with the request
+  def self.get(path, query=nil, data=nil, headers=nil)
+    perform_request('GET', path, query, data, headers)
+  end
+
+  # @method post
+  #   Perform a POST request.
+  # @param path
+  #   The path to perform a POST request against
+  # @param query
+  #   The query to add to the URL
+  # @param data
+  #   The data to send with the request
+  # @param headers
+  #   The headers to send with the request
+  def self.post(path, query=nil, data=nil, headers=nil)
+    perform_request('POST', path, query, data, headers)
+  end
+
+  # @method perform_request
+  #   Perform a HTTP request.
+  # @param type
+  #   The HTTP request type
+  # @param path
+  #   The path to perform the request against.
+  # @param data
+  #   The data to send with the request
+  # @param headers
+  #   Hash with headers to add to the request
+  private
+  def self.perform_request(type, path, query=nil, data=nil, headers=nil)
+    url = parse_url(path, query)
+    case type.upcase
+    when 'GET'
+      request = Net::HTTP::Get.new(url.request_uri)
+    when 'POST'
+      request = Net::HTTP::Post.new(url.request_uri)
+    else
+      raise Puppet::Error, "Unsupported HTTP request type: #{type}"
+    end
+    request.content_type = 'application/json'
+    request.body = data
+    if headers then
+      headers.each do |key, value|
+        if !value.is_a?(String) then
+          val = value.to_s
+        else
+          val = value
+        end
+
+        request[key] = val
+      end
+    end
+    response = Net::HTTP.start(url.host, url.port,
+                               :use_ssl => url.scheme == 'https') do |http|
+      http.request(request)
+    end
+    response
+  end
+end
---
+++ spec/unit/provider/influxdb_spec.rb
@@ -0,0 +1,81 @@
+# Copyright (C) 2018 Binero
+#
+# Author: Tobias Urdin <tobias.urdin@binero.se>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+require 'spec_helper'
+require 'puppet/provider/influxdb'
+
+describe Puppet::Provider::InfluxDB do
+  describe '#parse_url' do
+    it 'should parse url' do
+      Puppet::Provider::InfluxDB.expects(:base_url).returns('http://localhost:8086').at_most(4)
+      url1 = described_class.parse_url('first_url')
+      url2 = described_class.parse_url('/second_url')
+      url3 = described_class.parse_url('third_url', 'q=test query')
+      url4 = described_class.parse_url('fourth_url', 'q=test1&w=test2 test3')
+      exp1 = URI::parse('http://localhost:8086/first_url')
+      expect(url1).to eq(exp1)
+      exp2 = URI::parse('http://localhost:8086/second_url')
+      expect(url2).to eq(exp2)
+      exp3 = URI::parse('http://localhost:8086/third_url?q=test%20query')
+      expect(url3).to eq(exp3)
+      exp4 = URI::parse('http://localhost:8086/fourth_url?q=test1&w=test2%20test3')
+      expect(url4).to eq(exp4)
+    end
+  end
+
+  describe '#query' do
+    it 'should do a query' do
+      described_class.expects(:post).with('query', 'q=query here').returns(true)
+      expect(described_class.query('query here')).to be_truthy
+    end
+  end
+
+  describe '#get' do
+    it 'should do a get' do
+      described_class.expects(:perform_request).with('GET', 'query', 'q=test', nil, nil).returns(true)
+      expect(described_class.get('query', 'q=test')).to be_truthy
+    end
+  end
+
+  describe '#post' do
+    it 'should do a post' do
+      described_class.expects(:perform_request).with('POST', 'query', 'q=test', nil, nil).returns(true)
+      expect(described_class.post('query', 'q=test')).to be_truthy
+    end
+  end
+
+  describe '#perform_request' do
+    before :each do
+      Puppet::Provider::InfluxDB.expects(:base_url).returns('http://localhost:8086')
+    end
+
+    it 'should do a default get' do
+      Net::HTTP.expects(:start).returns(true)
+      response = described_class.perform_request('GET', 'path')
+      expect(response).to eq(true)
+    end
+
+    it 'should do a get with query, data and headers' do
+      Net::HTTP.expects(:start).returns(true)
+      response = described_class.perform_request('GET', 'path', 'q=query', 'data', {'my' => 'header'})
+      expect(response).to eq(true)
+    end
+
+    it 'should fail' do
+      expect { described_class.perform_request('what', 'path') }.to raise_error(Puppet::Error)
+    end
+  end  
+end
